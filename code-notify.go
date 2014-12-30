92447a8d674cc82c3979061baac70ad8dd6b867f
bmallred <bryan.allred@gmail.com>
2014-12-30 11:41:55 -0500

    removed init() and added ignore file

--- code-notify.go
+++ code-notify.go
@@ -5,41 +5,9 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"strings"
 
 	"code.revolvingcow.com/revolvingcow/code/cmd"
 )
-
-func init() {
-	for _, vcs := range cmd.GetVersionControlSystems() {
-		key := fmt.Sprintf("CODE_%s_INCOMING", strings.ToUpper(vcs))
-		env := os.Getenv(key)
-
-		// Temporarily define the subcommand `incoming` for known version control systems
-		if env == "" {
-			incoming := ""
-
-			switch vcs {
-			case "git":
-				incoming = "log ..@{u}"
-				break
-			case "hg":
-				incoming = "incoming"
-				break
-			case "bzr":
-				incoming = "missing"
-				break
-			case "tf":
-				incoming = ""
-				break
-			}
-
-			if incoming != "" {
-				os.Setenv(key, incoming)
-			}
-		}
-	}
-}
 
 func main() {
 	filepath.Walk(cmd.GetWorkingDirectory(), walk)
@@ -56,8 +24,8 @@ func walk(path string, fileInfo os.FileInfo, e error) error {
 	app := &cmd.App{
 		Directory: path,
 		Args:      []string{"incoming"},
-		Stdout:    &buffer,
-		Stderr:    &buffer,
+		//Stdout:    &buffer,
+		Stderr: &buffer,
 	}
 
 	return app.Run()
